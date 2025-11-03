f3423ed7e3eb7e25e3a167bdf02f823277f90082
Daniel <101250976+Gnarfhead@users.noreply.github.com>
2025-11-03 08:00:59 +0000

    anpassungen roll

--- src/sw_character_generator/functions/roleDice.py
+++ src/sw_character_generator/functions/roleDice.py
@@ -1,11 +1,22 @@
 import random
 
-def wuerfle_3d6():
-    """Würfelt 3 sechsseitige Würfel und gibt die Summe und Einzelwürfe zurück."""
-    wuerfe = [random.randint(1, 6) for _ in range(3)]
+def wuerfle_3d6(dropLow=None):
+    """Würfelt 4 sechsseitige Würfel und gibt die Summe und Einzelwürfe zurück."""
+    
+    if dropLow is None:
+        print(f"Drop lowest ist inaktiv")
+        wuerfe = [random.randint(1, 6) for _ in range(3)] 
+        print(f"Würfe: {wuerfe}")       
+    else:    
+        print(f"Drop lowest ist aktiv")
+        wuerfe = [random.randint(1, 6) for _ in range(4)] 
+        print(f"Würfe: {wuerfe}") 
+        wuerfe.remove(min(wuerfe))  
+                  
     summe = sum(wuerfe)
-    return summe
+    return summe, wuerfe
 
 # Beispielaufruf
-gesamt = wuerfle_3d6()
-print(f"Summe: {gesamt}")
+summe, gesamt = wuerfle_3d6(dropLow=None)
+print(f"Würfe: {gesamt}")
+print(f"Summe: {summe}")
