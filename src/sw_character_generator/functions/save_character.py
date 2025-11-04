77924e2afc5d29edb8519e136848b8ab897f14d2
Daniel <101250976+Gnarfhead@users.noreply.github.com>
2025-11-04 09:03:40 +0000

    save charakter

---
+++ src/sw_character_generator/functions/save_character.py
@@ -0,0 +1,10 @@
+import json, os
+
+def save_character(character_data: dict):
+    
+    filename = f"{character_data['characterName']}.json"
+    path = os.path.join("saved_data", filename)
+    os.makedirs(os.path.dirname(path), exist_ok=True)
+    with open(path, "w") as f:
+        json.dump(character_data, f, indent=4)
+    print(f"File saved at: {path}")
--- src/sw_character_generator/main.py
+++ src/sw_character_generator/main.py
@@ -9,20 +9,18 @@ if __name__ == "__main__":
 from dataclasses import make_dataclass, asdict
 from classes.character import Character
 from classes.fighter import Fighter
+from functions.save_character import save_character
 
 #def main():
 player_character = Character(playerName="Alice", characterName="Thorin")
-#player_class = Fighter(playerClass="Fighter")
 player_class = Fighter(playerClass="Fighter")
 
 merged_character = asdict(player_class) | asdict(player_character)
 FinalPlayerCharacter = make_dataclass("FinalPlayerCharacter", merged_character.keys())
 final_character_instance = FinalPlayerCharacter(**merged_character)
+print(asdict(final_character_instance))
 
-print(final_character_instance)
-print(f"Character Details: {final_character_instance.characterName}, Class: {final_character_instance.playerClass}")
-print(final_character_instance.characterName, final_character_instance.playerClass)
+save_character(asdict(final_character_instance))
 
 
-    
 
