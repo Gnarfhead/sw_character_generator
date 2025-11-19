0ec815ed7e1626120fb0d600f36d4635062b92d7
Daniel Theuerkauff <dtheuerk@proton.me>
2025-11-19 21:33:17 +0100

    feat: add spell level attributes for cleric, druid, and wizard classes

--- src/sw_character_generator/classes/profession/cleric.py
+++ src/sw_character_generator/classes/profession/cleric.py
@@ -29,6 +29,14 @@ def apply_cleric_dependent_modifiers(character):
     character.xp_bonus = 0
     character.parry = 0
     character.spells_lvl1 = 0
+    character.spells_lvl2 = 0
+    character.spells_lvl3 = 0
+    character.spells_lvl4 = 0
+    character.spells_lvl5 = 0
+    character.spells_lvl6 = 0
+    character.spells_lvl7 = 0
+    character.spells_lvl8 = 0
+    character.spells_lvl9 = 0
 
     # Calculate total HP
     character.hp = wuerfle_1d6(1) + character.hp_mod
